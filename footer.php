8d655695ca2cfa35f477aa60a02be3da0fbd0ea2
Chano Vera <contacto@chanovera.com>
2023-08-24 11:20:24 -0600

    se crea el archivo footer.php

---
+++ footer.php
@@ -0,0 +1,9 @@
+<?php
+        echo '<footer id="main-footer" class="container">
+            <div class="footer-content section">' .
+                
+            '</div>
+        </footer>' .
+        wp_footer() .
+    '</body>
+</html>';
