4568017a81f6a22ecb5d39438f06f84e08097ab5
Chano Vera <contacto@chanovera.com>
2023-08-24 11:25:45 -0600

    se agrega el header.js al final del footer desde functions.php

--- functions.php
+++ functions.php
@@ -9,3 +9,12 @@ function load_parts_header() {
      
 }
 add_action( 'wp_enqueue_scripts', 'load_parts_header' );
+
+// Carga componentes en el footer
+function load_parts_footer(){
+
+    // JS de efectos en la cabecera
+    wp_enqueue_script( 'header', get_template_directory_uri() . '/assets/js/header.js', '', 1, true );
+    
+}
+add_action( 'get_footer', 'load_parts_footer' );
