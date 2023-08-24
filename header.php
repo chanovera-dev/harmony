842c95255d6f6955489351969d57df71f4cb6451
Chano Vera <contacto@chanovera.com>
2023-08-24 12:22:40 -0600

    se corrigieron errores de sintaxis en header.php

--- header.php
+++ header.php
@@ -1,16 +1,15 @@
-<?php 
-echo '<!DOCTYPE html>' .
-'<html' . language_attributes() . '>' .
-    '<head>' .
-        '<meta charset="' . bloginfo( 'charset' ) . '">' .
-        '<meta name="description" content="' . bloginfo( 'description' ) . '">' .
-        '<meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">' .
-        wp_head() .
-    '</head>
-    <body ' . body_class() . '>' .
-        '<header class="main-header container">
-            <section class="header-content section">' .
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+    <head>
+        <meta charset="<?php bloginfo( 'charset' ); ?>">
+        <meta name="description" content="<?php bloginfo( 'description' ); ?>">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <?php wp_head(); ?>
+    </head>
+    <body <?php body_class(); ?>>
+        <header class="main-header container">
+            <section class="header-content section">
                 
-            '</section>
-        </header>';
+            </section>
+        </header>
