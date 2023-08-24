9caf5dd5677026de0b90da007eb05d8d96d3d285
Chano Vera <contacto@chanovera.com>
2023-08-24 11:43:06 -0600

    se agrega el soporte para 'wc-product-gallery-zoom', 'wc-product-gallery-lightbox' y 'wc-product-gallery-slider' en woocommerce.php

--- functions/woocommerce.php
+++ functions/woocommerce.php
@@ -1,1 +1,9 @@
 <?php
+
+// Agrega soporte para woocommerce
+function soporte_woocommerce(){ add_theme_support( 'woocommerce' ); }
+add_action( 'after_setup_theme', 'soporte_woocommerce' );
+
+add_theme_support( 'wc-product-gallery-zoom' );
+add_theme_support( 'wc-product-gallery-lightbox' );
+add_theme_support( 'wc-product-gallery-slider' );
