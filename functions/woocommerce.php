9e1e250eaa3cec6a8c88df2a22d259a2f77fdc4f
Chano Vera <contacto@chanovera.com>
2023-08-24 14:57:02 -0600

    se edita woocomerce.php para que muestre el carrito en tiempo real

--- functions/woocommerce.php
+++ functions/woocommerce.php
@@ -21,5 +21,19 @@ function replace_star_ratings($html, $rating) {
 //Disable all woocommerce stylesheets
 add_filter( 'woocommerce_enqueue_styles', '__return_empty_array' );
 
+// Muestra el carrito en tiempo real
+add_filter( 'woocommerce_add_to_cart_fragments', 'woocommerce_header_add_to_cart_fragment' );
+function woocommerce_header_add_to_cart_fragment( $fragments ) {
+	global $woocommerce;
+	ob_start();
+	?>
+	<a class="cart-customlocation" href="<?php echo esc_url(wc_get_cart_url()); ?>" title="<?php _e('View your shopping cart', 'woothemes'); ?>">
+	    <?php echo sprintf(_n('%d', '%d', $woocommerce->cart->cart_contents_count, 'woothemes'), $woocommerce->cart->cart_contents_count);?>
+	</a>
+	<?php
+	$fragments['a.cart-customlocation'] = ob_get_clean();
+	return $fragments;
+}
+
 // Estilos particulares
 require_once(get_template_directory() . '/functions/woocommerce/components.php');
