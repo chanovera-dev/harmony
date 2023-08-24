c59fc412d04ec1f2d5d56db3306f74375356593b
Chano Vera <contacto@chanovera.com>
2023-08-24 15:38:12 -0600

    se establecen los primeros colores en colors.php y más estilos en style.css

--- functions/colors.php
+++ functions/colors.php
@@ -1,2 +1,15 @@
 <?php
 
+function theme_colors() {
+    ?>
+    <style>
+        :root{
+            /* generales */
+            --background-color-body:#663c6d;
+            -background-color-main-content:#000;
+            --color-header:#fff;
+        }
+    </style>
+    <?php
+}
+add_action('wp_head', 'theme_colors');
