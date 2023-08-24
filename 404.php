45c33e68996f4aa5b49c535d42e8a551fce03321
Chano Vera <contacto@chanovera.com>
2023-08-24 12:29:23 -0600

    se corrigieron errores de sintaxis en 404.php

--- 404.php
+++ 404.php
@@ -1,14 +1,16 @@
-<?php
-    get_header();
+<?php get_header(); ?>
 
-    echo '<main id="main">' . 
-    '<section class="container"><div class="section">' .
-    '<h1>404</h1>' .
-    '<div>
-        <h2>' . __('¡Oops! No se encontró la página.', 'harmony') . '</h2>
-        <p>' . __('Parece que no se encontró nada en este sitio. ¿Quizás deba probar uno de los links inferiores o una búsqueda?', 'harmony') . '</p>' .
-        get_search_form() .
-    '</div>' .
-    '</div></section></main>';
+<main id="main"> 
+    <section class="container">
+        <div class="section">
+            <h1>404</h1>
+            <div>
+                <h2><?php __('¡Oops! No se encontró la página.', 'harmony'); ?></h2>
+                <p><?php __('Parece que no se encontró nada en este sitio. ¿Quizás deba probar uno de los links inferiores o una búsqueda?', 'harmony'); ?></p>
+                <?php get_search_form(); ?>
+            </div>
+        </div>
+    </section>
+</main>
 
-    get_footer();
+<?php get_footer(); ?>
