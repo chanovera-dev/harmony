46585696ad3fbe757393c66badd5e18cb8889b10
Chano Vera <contacto@chanovera.com>
2023-08-25 13:16:38 -0600

    se agregó el soporte de estilos para page.php en 'functions/components.php'

--- functions/components.php
+++ functions/components.php
@@ -1,1 +1,9 @@
 <?php
+
+// Estilos para las páginas
+function page_styles() {
+    if ( is_page() ) {
+        wp_enqueue_style( 'page-styles', get_template_directory_uri() . '/assets/css/page.css' );
+    }
+}
+add_action( 'wp_enqueue_scripts', 'page_styles' );
