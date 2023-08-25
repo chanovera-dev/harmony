bb3276bf564b7be183b81461eccc9d45f08eef31
Chano Vera <contacto@chanovera.com>
2023-08-25 04:02:05 -0600

    ajustes css al menú para ocultar el botón de búsqueda y el botón de usuario a menos de 768px

--- header.php
+++ header.php
@@ -15,11 +15,11 @@
                         echo '<ul class="nav left">';
                             echo '<li>'; include(TEMPLATEPATH . '/parts/header/menu-button.php'); echo '</li>';
                             echo '<li id="menu-wrapper" class="menu-wrapper">'; include(TEMPLATEPATH . '/parts/header/menu.php'); echo '</li>';
-                            echo '<li>'; include(TEMPLATEPATH . '/parts/header/search-button.php'); echo '</li>';
+                            echo '<li class="responsive">'; include(TEMPLATEPATH . '/parts/header/search-button.php'); echo '</li>';
                         echo '</ul>';
                         include(TEMPLATEPATH . '/parts/header/brand.php');
                         echo '<ul class="nav right">';
-                                echo '<li><a href="' . get_home_url() . '/mi-cuenta">
+                                echo '<li class="responsive"><a href="' . get_home_url() . '/mi-cuenta">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
                                         <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
                                         <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
@@ -40,12 +40,12 @@
                     } else {
                         echo '<ul class="nav left">';
                             echo '<li>'; include(TEMPLATEPATH . '/parts/header/menu-button.php'); echo '</li>';
-                            echo '<li id ="menu-wrapper" class="menu-wrapper">'; include(TEMPLATEPATH . '/parts/header/menu.php'); echo '</li>';
-                            echo '<li>'; include(TEMPLATEPATH . '/parts/header/search-button.php'); echo '</li>';
+                            echo '<li id="menu-wrapper" class="menu-wrapper">'; include(TEMPLATEPATH . '/parts/header/menu.php'); echo '</li>';
+                            echo '<li class="responsive">'; include(TEMPLATEPATH . '/parts/header/search-button.php'); echo '</li>';
                         echo '</ul>';
                         include(TEMPLATEPATH . '/parts/header/brand.php');
                         echo '<ul class="nav right">';
-                                echo '<li><a href="' . get_home_url() . '/mi-cuenta">
+                                echo '<li class="responsive"><a href="' . get_home_url() . '/mi-cuenta">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
                                         <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
                                         <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
@@ -58,7 +58,8 @@
                                     </svg>
                                 </a>
                             </li>';
-                            echo '<li><a href=""></a></li>';
+                            echo '<li><a href="#"></a>
+                            </li>';
                         echo '</ul>';
                     }
                 ?>
