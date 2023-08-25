5c4ee5b94d3b224be5dc9612d3c7c11406410d3b
Chano Vera <contacto@chanovera.com>
2023-08-25 03:40:52 -0600

    se crea 'icons.php' en 'functions/' para enlazar los iconos a los menús

---
+++ functions/icons.php
@@ -0,0 +1,13 @@
+<?php
+
+function theme_icons() {
+    ?>
+        <style>
+            .contact .menu li a[href*="facebook"]:before{content: ''; background-image: url('<?php echo get_template_directory_uri(); ?>/assets/icons/facebook-white.svg');}
+            .contact .menu li a[href*="mail"]:before{content: ''; background-image: url('<?php echo get_template_directory_uri(); ?>/assets/icons/envelope-white.svg');}
+            .contact .menu li a[href*="tel"]:before{content: ''; background-image: url('<?php echo get_template_directory_uri(); ?>/assets/icons/telephone-fill-white.svg');}
+            .contact .menu li a[href*="youtube"]:before{content: ''; background-image: url('<?php echo get_template_directory_uri(); ?>/assets/icons/youtube-white.svg');}
+        </style>
+    <?php
+}
+add_action('wp_head', 'theme_icons');
