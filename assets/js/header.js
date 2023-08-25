d5eb5b354ae156d763d947a6524481f19c6975e9
Chano Vera <contacto@chanovera.com>
2023-08-25 05:25:28 -0600

    ajustes js a la animación del menú

--- assets/js/header.js
+++ assets/js/header.js
@@ -9,7 +9,7 @@ function myFunction(x) {
         nav.className = "menu-wrapper active inactive";
         setTimeout(function(){
           nav.className = "menu-wrapper";
-        }, 1000);
+        }, 3000);
     }
 }
 
