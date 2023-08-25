aa3c9012d7e5ec243b99dcc714eabbc17f35a938
Chano Vera <contacto@chanovera.com>
2023-08-25 07:11:31 -0600

    se agrega contenido al footer

--- footer.php
+++ footer.php
@@ -1,7 +1,35 @@
 <?php
         echo '<footer id="main-footer" class="container">
             <div class="footer-content section">' .
-                
+                wp_nav_menu(
+                    array(
+                        'container' => 'nav', 
+                        'container_class' => 'about-us', 
+                        'theme_location' => 'about-us',
+                    ) 
+                );
+                wp_nav_menu(
+                    array(
+                        'container' => 'nav', 
+                        'container_class' => 'support', 
+                        'theme_location' => 'support',
+                    ) 
+                );
+                wp_nav_menu(
+                    array(
+                        'container' => 'nav', 
+                        'container_class' => 'politicals', 
+                        'theme_location' => 'politicals',
+                    ) 
+                );
+                wp_nav_menu(
+                    array(
+                        'container' => 'nav', 
+                        'container_class' => 'contact', 
+                        'theme_location' => 'contact',
+                    ) 
+                );
+                echo '<p>'.__('Todos los Derechos Reservados', 'renata').date("Y").' © '.bloginfo( 'title' ).'</p>';
             '</div>
         </footer>' .
         wp_footer() .
