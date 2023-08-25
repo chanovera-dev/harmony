61b5a85feec6c1be9b6b633f0ea36f96d1959892
Chano Vera <contacto@chanovera.com>
2023-08-24 21:34:09 -0600

    se agrega soporte para la animación del menú ranto en css como en javascript

--- assets/js/header.js
+++ assets/js/header.js
@@ -9,3 +9,30 @@ function myFunction(x) {
         nav.className = "menu-wrapper";
     }
 }
+
+// detecta el scroll en el sitio
+const body = document.body;
+const header = document.querySelector(".main-header");
+const menu = document.querySelector(".main-header .menu");
+const scrollUp = "scroll-up";
+const scrollDown = "scroll-down";
+let lastScroll = 0;
+
+window.addEventListener("scroll", () => {
+  const currentScroll = window.pageYOffset;
+  if (currentScroll <= 0) {
+    body.classList.remove(scrollUp);
+    return;
+  }
+  
+  if (currentScroll > lastScroll && !body.classList.contains(scrollDown)) {
+    // down
+    body.classList.remove(scrollUp);
+    body.classList.add(scrollDown);
+  } else if (currentScroll < lastScroll && body.classList.contains(scrollDown)) {
+    // up
+    body.classList.remove(scrollDown);
+    body.classList.add(scrollUp);
+  }
+  lastScroll = currentScroll;
+});
