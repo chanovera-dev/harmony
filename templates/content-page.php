ec10746fcf2e87a928d2a7095fa282d4bab3ab4c
Chano Vera <contacto@chanovera.com>
2023-08-25 13:09:15 -0600

    se creó page.php

---
+++ templates/content-page.php
@@ -0,0 +1,17 @@
+<main id="main">
+
+    <section class="container main-content">
+
+        <div class="section content-section">
+
+            <div class="content">
+
+                <?php the_content(); ?>
+
+            </div>
+
+        </div>
+
+    </section>
+
+</main>
