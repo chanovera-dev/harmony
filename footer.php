f6527a778b0dc9c7ebc4fefdc410b26b6b1219ed
Chano Vera <contacto@chanovera.com>
2023-08-25 08:09:15 -0600

    se agrega código php para obtener el nombre de los menús

--- footer.php
+++ footer.php
@@ -14,20 +14,32 @@
                         ) 
                     ); 
                 echo '</div>';
-                wp_nav_menu(
-                    array(
-                        'container' => 'nav', 
-                        'container_class' => 'support', 
-                        'theme_location' => 'support',
-                    ) 
-                );
-                wp_nav_menu(
-                    array(
-                        'container' => 'nav', 
-                        'container_class' => 'politicals', 
-                        'theme_location' => 'politicals',
-                    ) 
-                );
+                echo '<div>';
+                    $menu_id = get_nav_menu_locations()[ 'support' ];
+                    $menu = wp_get_nav_menu_object( $menu_id );
+                    $items = wp_get_nav_menu_items( $menu_id );
+                    echo '<div class="title-wrapper"><h2 class="title">' . $menu->name . '</h2></div>';
+                    wp_nav_menu(
+                        array(
+                            'container' => 'nav', 
+                            'container_class' => 'support', 
+                            'theme_location' => 'support',
+                        ) 
+                    );
+                echo '</div>';
+                echo '<div>';
+                    $menu_id = get_nav_menu_locations()[ 'politicals' ];
+                    $menu = wp_get_nav_menu_object( $menu_id );
+                    $items = wp_get_nav_menu_items( $menu_id );
+                    echo '<div class="title-wrapper"><h2 class="title">' . $menu->name . '</h2></div>';
+                    wp_nav_menu(
+                        array(
+                            'container' => 'nav', 
+                            'container_class' => 'politicals', 
+                            'theme_location' => 'politicals',
+                        ) 
+                    );
+                echo '</div>';
                 wp_nav_menu(
                     array(
                         'container' => 'nav', 
