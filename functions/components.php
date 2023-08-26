871ace7d6374a06e2cbf8a7467505e7995641acf
Chano Vera <contacto@chanovera.com>
2023-08-26 09:42:28 -0600

    se agregaron estilos en widgets.css, para la lista de dirección

--- functions/components.php
+++ functions/components.php
@@ -7,3 +7,13 @@ function page_styles() {
     }
 }
 add_action( 'wp_enqueue_scripts', 'page_styles' );
+
+// Estilos para el template de la página Contacto
+function contact_styles() {
+    if ( is_page_template('contact.php') ) {
+        wp_enqueue_style( 'contact-styles', get_template_directory_uri() . '/assets/css/contact.css' );
+        wp_enqueue_style( 'sections-styles', get_template_directory_uri() . '/assets/css/sections.css' );
+        wp_enqueue_style( 'widgets-styles', get_template_directory_uri() . '/assets/css/widgets.css' );
+    }
+}
+add_action( 'wp_enqueue_scripts', 'contact_styles' );
