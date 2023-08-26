5f75d8fcb3af4b7962e70fe99d3734e1afc686d0
Chano Vera <contacto@chanovera.com>
2023-08-26 12:36:07 -0600

    se agrega la primera persona del team en customizer.php

--- functions/customizer.php
+++ functions/customizer.php
@@ -7,12 +7,20 @@ function theme_customizer($wp_customize) {
         'description' => __('Establece los datos del equipo de trabajo'), 
         'priority' => 11,
     ));
-        // foto
-        $wp_customize->add_setting('picture_me', array(
-            'default' => get_bloginfo('template_url') . '/assets/img/avatar-06-2023.png',
+        // servicios -- titulo
+        $wp_customize->add_setting('team_title', array(
+            'default' => __('Equipo de trabajo'),
         ));
-        $wp_customize->add_control(new WP_Customize_Image_control($wp_customize, 'picture_me', array(
-            'label' => 'Mi foto',
+        $wp_customize->add_control('team_title', array(
+            'label' => 'Título',
+            'section' => 'team__data',
+        ));
+        // testimonios -- foto de testimonio 1
+        $wp_customize->add_setting('pic-testimony-1', array(
+            'default' => get_bloginfo('template_url') . '/assets/img/user.png',
+        ));
+        $wp_customize->add_control(new WP_Customize_Image_control($wp_customize, 'pic-testimony-1', array(
+            'label' => 'Persona 1',
             'section' => 'team__data',
         )));
 }
