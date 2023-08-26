87bae7c494cd97094ea5a9e8e374cc278f719408
Chano Vera <contacto@chanovera.com>
2023-08-26 12:07:14 -0600

    primeros datos de customizer.php

--- functions/customizer.php
+++ functions/customizer.php
@@ -7,5 +7,13 @@ function theme_customizer($wp_customize) {
         'description' => __('Establece los datos del equipo de trabajo'), 
         'priority' => 11,
     ));
+        // foto
+        $wp_customize->add_setting('picture_me', array(
+            'default' => get_bloginfo('template_url') . '/assets/img/avatar-06-2023.png',
+        ));
+        $wp_customize->add_control(new WP_Customize_Image_control($wp_customize, 'picture_me', array(
+            'label' => 'Mi foto',
+            'section' => 'team__data',
+        )));
 }
 add_action('customize_register', 'theme_customizer');
