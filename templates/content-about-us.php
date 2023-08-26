b629ddd9661fa5d750eecff61c925a07e58928ef
Chano Vera <contacto@chanovera.com>
2023-08-26 14:35:53 -0600

    ajustes a content-about-us.php

--- templates/content-about-us.php
+++ templates/content-about-us.php
@@ -67,7 +67,7 @@
         <div class="section content-section">
 
             <div class="content">
-
+            <div class="title-wrapper"><h2 class="title"><?php echo get_theme_mod('reviews_title', 'Revisiones de clientes'); ?></h2></div>
                 <?php if ( is_active_sidebar('reviews') ) { dynamic_sidebar('reviews');} ?>
 
             </div>
