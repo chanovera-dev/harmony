1bd642329e4ce35c107f531f60c666ef96fc9e8b
Chano Vera <contacto@chanovera.com>
2023-08-26 20:31:35 -0600

    se crea content-archive.php

---
+++ templates/content-archive.php
@@ -0,0 +1,23 @@
+<article class="article">
+
+        <?php if ( has_post_thumbnail() == false ) : ?>
+            <img class="thumbnail" src="<?php echo get_template_directory_uri(); ?>/assets/img/blog.jpg" alt="Imagen del artículo" loading="lazy" width="300" height="200">
+        <?php else: ?>
+            <img class="thumbnail" src="<?php the_post_thumbnail_url( 'media' ); ?>" alt="Imagen del artículo" loading="lazy" width="300" height="200">
+        <?php endif; ?>
+
+        <div class="article__content">
+            <?php
+                include(TEMPLATEPATH . '/parts/widgets/author.php');
+            ?>
+                <?php echo the_category(); ?>
+                <a class="permalink" href="<?php the_permalink() ?>" target="_blank">
+                    <?php the_title( '<h3 class="article__title">', '</h3>' ); ?>
+                </a>
+            <?php
+                the_excerpt();
+                include(TEMPLATEPATH . '/parts/widgets/publicate-date.php');
+            ?>
+        </div>
+
+</article>
