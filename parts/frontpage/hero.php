e45fe7c60e5664e6436dd0ab06b5602341523778
Chano Vera <contacto@chanovera.com>
2023-08-27 11:03:05 -0600

    primeros datos en hero.php

--- parts/frontpage/hero.php
+++ parts/frontpage/hero.php
@@ -1,3 +1,34 @@
-<section class="container">
-    <div class="section"></div>
+<section id="hero" class="container">
+    <div class="section">
+        <h1>¡Vive en bienestar y armonía!</h1>
+        <h2>Reconect mente y espíritu</h2>
+        <div>
+            <div>
+                <a href="http://">VIDEO DE DEMOSTRACIÓN</a>
+                <h2>Nuestra historia</h2>
+                <p>Agencia especializada en ayudar a tener una vida más sana</p>
+                <a href="http://">
+                    Saber más
+                </a>
+            </div>
+            <figure>
+                <img src="" alt="">
+                <figcaption><a href="http://">Saber más</a></figcaption>
+            </figure>
+            <div>
+                <div>
+                    <h2>12 <span>+</span></h2>
+                    <p>Años de experiencia</p>
+                </div>
+                <div>
+                    <h2>800 <span>+</span></h2>
+                    <p>Proyectos realizados</p>
+                </div>
+                <div>
+                    <h2>500 <span>+</span></h2>
+                    <p>Clientes felices</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
