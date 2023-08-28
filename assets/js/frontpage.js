f6a5c75fd148c29a15bd023a7122741d8835ccf3
Chano Vera <contacto@chanovera.com>
2023-08-28 07:24:28 -0600

    estilos y javascript para transformar texto en un círculo

--- assets/js/frontpage.js
+++
@@ -1,6 +0,0 @@
-circle = document.getElementById("circle");
-circlearray = circle.textContent.split(' ');
-circle.textContent = ' ';
-for(var i = 0; i < circlearray.length; i++){
-    circle.innerHTML += '<span style="transform:rotate('+((i+1)*10)+'deg)"></span>' + circlearray[i] + '</span>'
-}
