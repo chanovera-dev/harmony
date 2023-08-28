6851d030d7acbbdb54ad983e2cf04064bca32784
Chano Vera <contacto@chanovera.com>
2023-08-28 10:12:28 -0600

    'parts/frontpage/sitemap.php'

--- front-page.php
+++ front-page.php
@@ -5,6 +5,7 @@ get_header();
 echo '<main id="main">';
 
 include(TEMPLATEPATH . '/parts/frontpage/hero.php');
+include(TEMPLATEPATH . '/parts/frontpage/sitemap.php');
 
 echo '</main>';
 
---
+++ parts/frontpage/sitemap.php
@@ -0,0 +1,5 @@
+<section id="sitemap" class="container main-content">
+    <div class="section sitemap-section">
+        
+    </div>
+</section>
