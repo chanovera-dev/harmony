13e5328422f353fb416f8afaa9111b6e213fc4d7
Chano Vera <contacto@chanovera.com>
2023-08-28 10:51:20 -0600

    parts/frontpage/suscribe.php

---
+++ parts/frontpage/suscribe.php
@@ -0,0 +1,5 @@
+<section id="suscribe" class="container main-content">
+    <div class="section suscribe-section">
+        <?php do_shortcode('[contact-form-7 id="4f40a18" title="Suscribe"]'); ?> 
+    </div>
+</section>
