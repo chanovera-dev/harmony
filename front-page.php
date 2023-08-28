f8bd3d8c405cd1ed18c490e9c97b0508c0859524
Chano Vera <contacto@chanovera.com>
2023-08-28 12:43:38 -0600

    frontpage.php

--- front-page.php
+++ front-page.php
@@ -3,11 +3,10 @@
 get_header();
 
 echo '<main id="main">';
-
 include(TEMPLATEPATH . '/parts/frontpage/hero.php');
 include(TEMPLATEPATH . '/parts/frontpage/sitemap.php');
 include(TEMPLATEPATH . '/parts/frontpage/suscribe.php');
-
+include(TEMPLATEPATH . '/parts/sections/contact-group.php');
 echo '</main>';
 
 get_footer();
