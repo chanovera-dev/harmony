6844911e2b34dc2b8173e32ff4a9f51c639bc4c2
Chano Vera <contacto@chanovera.com>
2023-08-28 10:23:57 -0600

    'frontpage.css'

--- parts/frontpage/sitemap.php
+++ parts/frontpage/sitemap.php
@@ -1,5 +1,12 @@
 <section id="sitemap" class="container main-content">
     <div class="section sitemap-section">
-        
+        <ul >
+            <li>
+                <a href="">Página 1</a>
+                <ul>
+                    <li>Página 1 / 2</li>
+                </ul>
+            </li>
+        </ul>        
     </div>
 </section>
