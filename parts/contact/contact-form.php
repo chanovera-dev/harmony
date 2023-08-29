5b1dd701947d0a018543894a2ba4808d0d3ae1ed
Chano Vera <contacto@chanovera.com>
2023-08-29 14:20:08 -0600

    ajustes a contact-form.php y a suscribe.php

--- parts/contact/contact-form.php
+++ parts/contact/contact-form.php
@@ -1,3 +1,3 @@
 <div class="contact-form">
-    <?php echo do_shortcode('[contact-form-7 id="686df5f" title="Formulario de contacto 1"]'); ?>
+    <?php echo do_shortcode('[contact-form-7 id="6bc2045" title="Formulario de contacto 1"]'); ?>
 </div>
--- parts/frontpage/suscribe.php
+++ parts/frontpage/suscribe.php
@@ -1,6 +1,6 @@
 <section id="suscribe" class="container">
     <div class="section suscribe-section">
         <div class="title-wrapper"><h2 class="title">Suscríbete</h2></div>
-        <?php echo do_shortcode('[contact-form-7 id="4f40a18" title="Suscribe"]'); ?> 
+        <?php echo do_shortcode('[contact-form-7 id="8ddc90b" title="Suscribe"]'); ?> 
     </div>
 </section>
